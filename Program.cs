60c60a54a0d121fa214bbb176233a0cb868ddfe4
Isak Hauknes <isakhauknes@gmail.com>
2022-02-23 16:28:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chinook_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
