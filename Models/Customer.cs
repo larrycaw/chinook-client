4cfb42d8cd7aed20ad824b83bf3e87db9da2c711
Isak Hauknes <isakhauknes@gmail.com>
2022-02-23 16:37:15 +0100

    FEAT: Folders for models and repositories + Customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chinook_client.Models
+{
+    public class Customer
+    {
+        /// <summary>
+        /// Property(Id): Customers identification number.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Property(FirstName): First name of customer.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Property(LastName): Last name of customer.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Property(Country): Country where customer is located.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Property(PostalCode): Postal code of customers residence.
+        /// </summary>
+        public string PostalCode { get; set; }
+
+        /// <summary>
+        /// Property(PhoneNumber): Phone number of customer.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Property(Email): Email of customer
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
