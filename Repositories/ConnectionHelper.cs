5a5b4599ef69c8595db2620af87df8a48b42a541
Isak Hauknes <isakhauknes@gmail.com>
2022-02-23 16:41:57 +0100

    FEAT: Connection string

---
+++ Repositories/ConnectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chinook_client.Repositories
+{
+    public class ConnectionHelper
+    {
+        /// <summary>
+        /// SQL Server configuration
+        /// </summary>
+        /// <returns>Connection string</returns>
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = @"ISAK-DESKTOP\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true;
+            connectionStringBuilder.TrustServerCertificate = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
